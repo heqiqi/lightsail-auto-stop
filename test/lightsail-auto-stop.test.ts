4efdf0018907da566adc6e5de299ccc6f54654e9
He Qi <qihen@amazon.com>
2023-09-06 16:23:15 +0800

    Initial commit

---
+++ test/lightsail-auto-stop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LightsailAutoStop from '../lib/lightsail-auto-stop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lightsail-auto-stop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LightsailAutoStop.LightsailAutoStopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
